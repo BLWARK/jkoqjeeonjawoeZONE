39c497bf5ce552128aa641edb6058cfe0546a36c
Ronald <rozu0980@gmail.com>
2025-03-05 12:54:13 +0700

    adj pic

--- components/page-components/adv-sect/AdvEditor.jsx
+++ components/page-components/adv-sect/AdvEditor.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const AdvEditor = () => {
   return (
     <div className="bg-gray-100 w-[380px] 2xl:h-[380px] h-[350px] rounded-lg overflow-hidden relative">
-          <Image src="/ads1s.jpg" alt="Advertisement" fill  className="object-contain" />
+          <Image src="/Ads1s.jpg" alt="Advertisement" fill  className="object-contain" />
         </div>
   )
 }
--- components/page-components/adv-sect/AdvMostRead.jsx
+++ components/page-components/adv-sect/AdvMostRead.jsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 const AdvMostRead = () => {
   return (
     <div className="bg-gray-100 2xl:w-[350px] xl:w-[350px] lg:w-[300px] 2xl:h-[350px] xl:h-[350px] lg:h-[300px] h-[350px] rounded-lg overflow-hidden relative">
-          <Image src="/ads1s.jpg" alt="Advertisement" fill  className="object-fill" />
+          <Image src="/Ads1s.jpg" alt="Advertisement" fill  className="object-fill" />
           
         </div>
   )
