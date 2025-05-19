7d65828ea9e177d398011c2eea8dc7717e7f19a9
Ronald <rozu0980@gmail.com>
2025-05-19 11:05:30 +0700

    update new

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
+        protocol: "http", // karena `user.avatar` pakai http
+        hostname: "156.67.217.169",
+        port: "9001", // jika diperlukan
+        pathname: "/**", // untuk semua path
+      },
+      {
+        protocol: "https", // juga tambahkan jika image bisa dari https (optional)
         hostname: "storage.xyzone.media",
         pathname: "/**",
       },
@@ -10,4 +17,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig; // ← ini penting, jangan pakai `export default` kalau pakai JavaScript biasa
+module.exports = nextConfig;
