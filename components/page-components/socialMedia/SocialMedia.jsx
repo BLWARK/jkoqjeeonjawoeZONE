7c23a38270d9c1d34b95b7e09d4ec3ee25becf14
Ronald <rozu0980@gmail.com>
2025-05-21 11:47:29 +0700

    social media update

--- components/page-components/socialMedia/SocialMedia.jsx
+++ components/page-components/socialMedia/SocialMedia.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { FaFacebookF, FaInstagram, FaYoutube } from "react-icons/fa";
+import { FaInstagram, FaYoutube } from "react-icons/fa";
+import { AiFillTikTok } from "react-icons/ai";
 import { FaXTwitter } from "react-icons/fa6";
 
 const SocialMedia = () => {
@@ -8,18 +9,18 @@ const SocialMedia = () => {
       <h4 className="text-md font-bold text-red-600">Follow Us</h4>
       <div className="grid grid-cols-2 gap-4 mt-4">
         <a
-          href="https://facebook.com"
+          href="https://www.tiktok.com/@xyzonemedia?_t=ZS-8wDy2fTaIBE&_r=1"
           target="_blank"
           rel="noopener noreferrer"
-          aria-label="Facebook" /* ✅ Tambahkan label aksesibilitas */
+          aria-label="tiktok" /* ✅ Tambahkan label aksesibilitas */
           className="flex items-center justify-center bg-gray-200 hover:bg-blue-200 rounded-lg py-3 transition"
         >
-          <FaFacebookF className="text-blue-700 text-3xl" />
-          <span className="sr-only">Facebook</span>{" "}
+          <AiFillTikTok className="text-black text-3xl" />
+          <span className="sr-only">Tiktok</span>{" "}
           {/* ✅ Tambahkan teks tersembunyi untuk screen reader */}
         </a>
         <a
-          href="https://twitter.com"
+          href="https://x.com/xyzoneupdate?s=21"
           target="_blank"
           aria-label="twitter" 
           rel="noopener noreferrer"
@@ -28,7 +29,7 @@ const SocialMedia = () => {
           <FaXTwitter className=" text-3xl" />
         </a>
         <a
-          href="https://youtube.com"
+          href="https://www.youtube.com/@XYZoneTV"
           target="_blank"
           rel="noopener noreferrer"
           aria-label="Youtube" /* ✅ Tambahkan label aksesibilitas */
@@ -37,7 +38,7 @@ const SocialMedia = () => {
           <FaYoutube className="text-red-600 text-3xl" />
         </a>
         <a
-          href="https://instagram.com"
+          href="https://www.instagram.com/xyzonemedia?igsh=NzNoaTF3aDc3aDM2"
           target="_blank"
           rel="noopener noreferrer"
           aria-label="Instagram" /* ✅ Tambahkan label aksesibilitas */
