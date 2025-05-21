46b8323e8d103a8e5c139d835c8112f66f837405
Ronald <rozu0980@gmail.com>
2025-05-21 15:58:44 +0700

    update Ads

--- components/page-components/adv-sect/AdvBottomHead.jsx
+++ components/page-components/adv-sect/AdvBottomHead.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const AdvBottomHead = () => {
   return (
     <div className="bg-gray-100 w-full 2xl:h-[300px] xl:h-[300px]  lg:h-[250px] h-[100px] rounded-lg overflow-hidden  relative">
-    <Image src="/SALE.jpg" alt="Advertisement" fill  className="object-contain" priority={true} />
+    <Image src="/1000x200 rgcc.png" alt="Advertisement" fill  className="object-contain" priority={true} />
   </div>
   )
 }
