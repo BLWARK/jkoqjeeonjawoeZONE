780158d33d34b7b282794cef7536c2c1514fb8fd
Ronald <rozu0980@gmail.com>
2025-05-27 00:28:28 +0700

    update sitemap-tags

--- app/sitemap/route.js
+++ app/sitemap/route.js
@@ -1,24 +1,30 @@
 export async function GET() {
   const sitemapUrls = [
-    'https://xyzone.media/sitemap-articles',
-    'https://xyzone.media/sitemap-pages',
-    'https://xyzone.media/sitemap-regional',
-     "https://xyzone.media/sitemap-tags",
+    "https://xyzone.media/sitemap-articles",
+    "https://xyzone.media/sitemap-pages",
+    "https://xyzone.media/sitemap-regional",
+    "https://xyzone.media/sitemap-tags/0",
+    "https://xyzone.media/sitemap-tags/1",
+    "https://xyzone.media/sitemap-tags/2",
   ];
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
     <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${sitemapUrls.map(url => `
+      ${sitemapUrls
+        .map(
+          (url) => `
         <sitemap>
           <loc>${url}</loc>
           <lastmod>${new Date().toISOString()}</lastmod>
-        </sitemap>`).join('\n')}
+        </sitemap>`
+        )
+        .join("\n")}
     </sitemapindex>`;
 
   return new Response(xml, {
     headers: {
-      'Content-Type': 'application/xml',
-      'Cache-Control': 'no-store',
+      "Content-Type": "application/xml",
+      "Cache-Control": "no-store",
     },
   });
 }
