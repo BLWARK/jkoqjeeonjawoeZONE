f204a1a1d9fad638594088b83e222cd4a5837154
Ronald <rozu0980@gmail.com>
2025-06-02 15:12:47 +0700

    category color update

--- data/categoryColors.js
+++ data/categoryColors.js
@@ -7,6 +7,10 @@ const categoryColors = {
   crypto: "bg-purple-500",
   business: "bg-orange-500",
   "C-LEVEL": "bg-purple-500", // Gunakan string agar mendukung karakter "-"
+  Wisata: "bg-green-500",
+  Kuliner: "bg-orange-500",
+  Budaya: "bg-pink-500",
+  Hukrim: "bg-red-500",
   DEFAULT: "bg-gray-500",
 };
 
