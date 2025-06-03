112013f6f197de57ccf5940bd48d93a0dbe7f36d
Ronald <rozu0980@gmail.com>
2025-06-03 09:41:05 +0700

    update google search console

--- app/layout.tsx
+++ app/layout.tsx
@@ -71,7 +71,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta
           name="google-site-verification"
-          content="l_arQzQREnB3JrRFH_koj6XNhCI2bUasKzT2pSH7w7g"
+          content="RSAlJodOSUpPQMKD9FJNeaE4D7evrXCbQ3mmBIl5LeQ"
         />
         <meta name="msvalidate.01" content="98F79C92B69C4284488C28E54D68FABC" />
         <link rel="icon" href="/favicon.ico" />
